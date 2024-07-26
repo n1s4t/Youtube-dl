e5df7aae02fad84076b7fd34f795763af6dde307
DecenT <61790940+n1s4t@users.noreply.github.com>
2024-07-27 02:05:12 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+clear
+sleep 3
+cd
+echo -e '\033[91m'
+echo ' ___           _        _ _ _              '
+echo '|_ _|_ __  ___| |_ __ _| | (_)_ __   __ _  '
+echo ' | ||  _ \/ __| __/ _  | | | |  _ \ / _  | '
+echo ' | || | | \__ \ || (_| | | | | | | | (_| | '
+echo '|___|_| |_|___/\__\__,_|_|_|_|_| |_|\__, | '
+echo '                                    |___/  '
+echo -e '\033[00m'
+echo -e '\033[34mCreated by: n1s4t'
+echo  ''  
+echo -e '\033[92m• \033[0m GitHub \033[91m :\033[36mhttps://www.github.com/n1s4t'
+echo -e '\033[92m• \033[0mTelegram \033[91m:\033[36mhttps://www.t.me/n1s4t'
+echo  ''
+sleep 3
+apt --assume-yesgit tsu python wpa-supplicant pixiewps iw wget
