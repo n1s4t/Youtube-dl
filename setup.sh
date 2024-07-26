98826c93dc4bda8694021c06dcf12072fda267a1
DecenT <61790940+n1s4t@users.noreply.github.com>
2024-07-27 03:47:08 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ apt update > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1
 echo -e '\033[93mInstalling Root-Repo'
 pkg install -y root-repo > /dev/null 2>&1
 echo -e '\033[93mInstalling Other Packages'
-apt --assume-yes install tsu python wpa-supplicant pixiewps iw > /dev/null 2>&1
+apt --assume-yes install tsu git python wpa-supplicant pixiewps iw > /dev/null 2>&1
 pkg install wget -y > /dev/null 2>&1
 cd
 mkdir .wifi
